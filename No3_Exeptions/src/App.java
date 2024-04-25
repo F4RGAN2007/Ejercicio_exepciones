319aefcfd599921dad176989ae4c1f9b84171fc7
F4RGAN2007 <avalo.santiago@correounivalle.edu.co>
2024-04-25 11:50:04 -0500

    Primer vistaso

---
+++ No3_Exeptions/src/App.java
@@ -0,0 +1,12 @@
+/* Descripcion: Programa que permite buscar y reservar vuelos
+ * Autor: Santiago Avalo Monsalve
+ * Fechad de creacion: 25-04-2023
+ * Version: 1.0
+ */
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
